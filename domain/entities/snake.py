250de53280da148633c191f28e5f265522360e43
hitoshi <npchitoshi@gmail.com>
2026-05-12 23:19:25 -0300

    feat: melhorias na classe snake

--- domain/entities/snake.py
+++ domain/entities/snake.py
@@ -5,16 +5,31 @@ from domain.bestiario import SnakesNames
 @dataclass
 class Snake:
     numero: int
-    hp: HP
+    _hp: HP
     especie: str = field(init=False)
     apelido: str = None
     status: str = "VIVO"
 
+    @property
+    def hp(self):
+        return self._hp
+
+    @hp.setter
+    def hp(self, novo_hp: HP):
+        if (self.status == "VIVO"):
+            self._hp = novo_hp
+        self._verifica_se_morreu()
+
     def __post_init__(self):
+        if self._hp.atual == 0:
+            raise ValueError("Não é permitido criar uma Snake morta")
+
         self.especie = SnakesNames.get(self.numero, "Espécie Desconhecida")
+        self._verifica_se_morreu()
 
     def receber_golpe(self, dano: int) -> None:
         self.hp = self.hp.receber_dano(dano)
 
-        if self.hp.atual == 0:
+    def _verifica_se_morreu(self):
+        if (self._hp.atual == 0):
             self.status = "MORTO"
