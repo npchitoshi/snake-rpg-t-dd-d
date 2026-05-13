912518fe9d081431fb4e658d9dbf4fa3b926f4b1
hitoshi <npchitoshi@gmail.com>
2026-05-12 23:19:48 -0300

    feat: subindo teste de snake

--- test/unit/domain/test_snake.py
+++ test/unit/domain/test_snake.py
@@ -1,15 +1,28 @@
+import pytest
 from domain.entities.snake import Snake
 from domain.value_objects.hp import HP
 
-
 def test_criar_snake_com_sucesso():
     hp = HP(atual=10, maximo=10)
-    snake = Snake(numero=1, hp=hp)
+    snake = Snake(numero=1, _hp=hp)
 
     assert snake.hp == hp
     assert snake.status == "VIVO"
     assert snake.especie is not None
 
-# def test_snake_falha_ao_ser_criada_sem_hp():
+def test_snake_falha_ao_ser_criada_com_hp_zero():
+    hp_zero = HP(atual=0, maximo=100)
 
-# def test_snake_deve_morrer_quando_hp_chegar_a_zero():
+    with pytest.raises(ValueError, match="Não é permitido criar uma Snake morta"):
+        Snake(numero=1, _hp=hp_zero)
+
+def test_snake_deve_morrer_quando_hp_chegar_a_zero():
+    hp = HP(atual=10, maximo=10)
+    snake = Snake(numero=1, _hp=hp)
+
+    hp_apos_dano = snake.hp.receber_dano(10)
+
+    snake.hp = hp_apos_dano
+
+    assert snake._hp.atual == 0
+    assert snake.status == "MORTO"
